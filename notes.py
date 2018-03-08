a1363ac4477c7d7276e211a902ae57c5d5a34698
mrjohnnychang <jch@csu.fullerton.edu>
2018-03-07 16:43:04 -0800

    Update notes.py

--- notes.py
+++ notes.py
@@ -407,3 +407,41 @@ print remove_duplicates([1, 1, 2, 2])
     return (l[len(l)/2] + l[((len(l)-1)/2)]) / 2.0
 #test
 print median([7, 3, 1, 4])
+
+
+#calcualte average:
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+print "This is all the grades: "
+def print_grades(grades_input):
+  for grade in grades_input:
+    print grade
+print_grades(grades)
+    
+def grades_sum(scores):
+  total = 0
+  for score in scores: 
+    total += score
+  return total
+print "The sum is: ", grades_sum(grades)
+
+def grades_average(grades_input):
+  return (grades_sum(grades_input) / float(len(grades_input)))
+print "The average is: ", grades_average(grades)
+
+def grades_variance(scores):
+  average = grades_average(scores)
+  variance = 0
+  for i in scores:
+    #computes squared difference
+    variance = variance + ((average - i) ** 2)
+  return (variance / float(len(scores)))
+print "The variance is: ", grades_variance(grades)
+
+def grades_std_deviation(variance):
+  return variance ** 0.5
+
+variance = grades_variance(grades)
+print "The standard deviation is: ", grades_std_deviation(variance)
+
+
